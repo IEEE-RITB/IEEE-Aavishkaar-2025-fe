337258a5f88b0982bda1ebeb52e2252cacf6beed
Manjunath3155 <manjunathpatil3155@gmail.com>
2025-03-13 16:34:13 +0530

    Added About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Events from "./pages/Events";
 import EventDetail from "./components/EventDetail";
 import Home from "./pages/Home";
@@ -6,6 +6,7 @@ import Sponsors from "./pages/Sponsors";
 import Schedule from "./pages/Schedule";
 import Team from "./pages/Team";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
 import { BaseLayout } from "./layouts/base-layout";
 import ScrollToTop from "./utils/scroll-to-top";
 
@@ -15,6 +16,7 @@ export default function App() {
       <BaseLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/events" element={<Events />} />
           <Route path="/events/:id" element={<EventDetail />} />
           <Route path="/sponsors" element={<Sponsors />} />
@@ -22,7 +24,7 @@ export default function App() {
           <Route path="/team" element={<Team />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <ScrollToTop/>
+        <ScrollToTop />
       </BaseLayout>
     </Router>
   );
--- src/layouts/base-layout.jsx
+++ src/layouts/base-layout.jsx
@@ -2,11 +2,11 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export function BaseLayout({ children }) {
-    return (
-        <main className="min-h-screen w-full bg-[#0D0D1A] text-white flex flex-col">
-            <Navbar/>
-            {children}
-            <Footer/>
-        </main>
-    )
+  return (
+    <main className="min-h-screen w-full bg-[#0D0D1A] text-white flex flex-col">
+      <Navbar />
+      {children}
+      <Footer />
+    </main>
+  );
 }
