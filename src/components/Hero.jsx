85f79240d2bf6cc46cbbe08758bf93ddd663c045
Manjunath Patil <65996426+Manjunath3155@users.noreply.github.com>
2025-03-17 18:29:01 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,17 @@ import { ArrowRight } from "lucide-react";
 
 const Hero = () => {
   return (
-    <section className="relative flex-1 w-full bg-[#1A0F33] md:rounded-lg md:overflow-hidden">
+    <section className="relative flex-1 w-full bg-[#1A0F33] rounded-md md:rounded-lg md:overflow-hidden border border-white/20 overflow-hidden">
       <div className="absolute inset-0 z-0 w-full">
         <img
           src="/placeholder.svg?height=600&width=1200"
           alt="Event background"
           className="h-full w-full object-cover opacity-40"
         />
-        <div className="absolute inset-0 w-full bg-[#1A0F33]"></div>
+        <div className="absolute inset-0 w-full bg-gradient-to-r from-[#0D0D1A]/80 to-[#4F33B3]/30 "></div>
       </div>
       <div className="relative z-10 px-4 py-16 h-full sm:py-20 w-full">
-        <div className="mb-6 pt-40 text-center">
+        <div className="mb-6 pt-34 sm:pt-40 text-center">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold tracking-wider">
             Aavishkaar'25
           </h1>
